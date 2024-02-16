1403767a952d44521149563cdd01dd9a345353db
Yaroslav Bilokrylets <4believy@gmail.com>
2024-02-16 12:44:51 +0200

    feat: add prompt creation

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -1,5 +1,56 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import Form from '@components/Form';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
 type Props = {};
+
+export type Post = {
+  prompt: string;
+  tag: string;
+};
+
 const CreatePrompt = (props: Props) => {
-  return <div>page</div>;
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState<Post>({ prompt: '', tag: '' });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const res = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (res.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
 };
 export default CreatePrompt;
